35f315f8971d9ceec8bed460a64281b386f9f587
ParmarShraddha <139035573+ParmarShraddha@users.noreply.github.com>
2024-07-10 00:41:10 +0530

    description

---
+++ Client_side/spotify_client/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_client/core/theme/theme.dart';
+
+import 'features/auth/view/pages/signup_page.dart';
+// import 'package:spotify_client/view/pages/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: AppTheme.darkThemeMode,
+      home: const SignupPage(),
+    );
+  }
+}
